1d0c83fe7e1475a565b79ca14c8632d27cf87e43
Michael Suggs <mjs3607@uncw.edu>
2017-05-25 16:10:45 -0400

    Restructured and rewrote project.
    Input now works by the user passing in an Agave-compatible link and their desired GWAS operations, and the pipeline will then generate all needed JSONs and submit as necessary.

--- DevelopingRelease/PipelineDevel/JsonBuilder.py
+++ DevelopingRelease/PipelineDevel/JsonBuilder.py
@@ -2,55 +2,146 @@ import json
 
 __author__ = "Michael J. Suggs // mjs3607@uncw.edu"
 
-class JsonBuilder:
-    """Summary of class here.
 
-    Longer class information....
-    Longer class information....
-
-    Attributes:
-        likes_spam: A boolean indicating if we like SPAM or not.
-        eggs: An integer count of the eggs we have laid.
+def fastlmm_json(data_name, input_dict):
     """
 
-    def __init__(self):
-        pass
-
-    def make_gwas_json(self, type):
-        """Makes an Agave-API compatible JSON for running jobs.
-
-        :param type: The type of GWAS JSON to generate.
-        :return:
-        """
-        pass
-
-    def fastlmm_json(self):
-        pass
-
-    def make_winnow_json(self, gwas_name, gwas_folder, ote_file):
-        json_text = {
-            "jobName" : gwas_name,
-            "softwareName" : "Winnow-0.9.2u1",
-            "requestedTime" : "02:00:00",
-            "archive" : "true",
-            "inputs" : {
-                "Class" : ote_file,
-                "Folder" : gwas_folder
-            },
-            "parameters" : {
-                "SNP": "SNP",
-                "Score": "P",
-                "Filename": "TestResults",
-                "beta": "BETA",
-                "threshold": "0.01",
-                "seper": "whitespace",
-                "kttype": "OTE"
+    :param data_folder:
+    :return:
+    """
+    json_text = {
+        "jobName": "FaST-LMM_{}".format(data_name),
+        "softwareName": "FaST-LMM-hpc-2.07u1",
+        "requestedTime": "02:00:00",
+        "archive": True,
+            "inputs": input_dict,
+            "parameters": {
+                "output": "full_results"
             }
         }
 
-        with open('{}.json'.format(gwas_name), 'w') as f:
-            json_text = json.dumps(json_text, f)
+    with open('FaST-LMM_{}.json'.format(data_name), 'w') as f:
+        json.dumps(json_text, f)
 
 
-if __name__ == '__main__':
+def ridge_json(data_name, input_dict):
+    json_text = {
+        "jobName": "RidgePredict_{}".format(data_name),
+        "softwareName": "RidgePredict-1.1u1",
+        "requestedTime": "00:30:00",
+        "archive": True,
+        "inputs": input_dict,
+        "parameters": {
+            "outputPed": "RidgePredict_{}.ped".format(data_name)
+        }
+    }
+
+    with open('RidgePredict_{}.json'.format(data_name), 'w') as f:
+        json.dumps(json_text, f)
+
+
+def bayesr_json(data_name, input_dict):
+    json_text = {
+        "jobName": "BayesR_{}".format(data_name),
+        "softwareName": "bayesR-2.00u1",
+        "processorsPerNode": 16,
+        "requestedTime": "48:00:00",
+        "memoryPerNode": 32,
+        "nodeCount": 16,
+        "archive": True,
+        "inputs": input_dict,
+        "parameters": {
+            "output": "BayesR_{}".format(data_name)
+        }
+    }
+
+    with open('BayesR_{}.json'.format(data_name), 'w') as f:
+        json.dumps(json_text, f)
+
+
+def plink_json(data_name):
+    json_text = {
+        "jobName": "Plink_{}".format(data_name),
+        "softwareName": "PLINK-hpc-1.07u2",
+        "processorsPerNode": 16,
+        "requestedTime": "01:00:00",
+        "memoryPerNode": 32,
+        "nodeCount": 1,
+        "archive": True,
+        "inputs": {
+            "file.list": data_name
+        },
+        "parameters": {
+            "out.basename": "success"
+        }
+    }
+
+    with open('PLINK_{}.json'.format(data_name), 'w') as f:
+        json.dumps(json_text, f)
+
+
+#TODO make current pipeline implementation work with this input format
+# def qxpak_json(data_name, input_dict):
+#     json_text = {
+#         "name": "QxPak_{}".format(data_name),
+#         "appId": "qxpak-stampede-5.05u2",
+#         "archive": True,
+#         "inputs": {
+#             "parameter_file": "agave://data.iplantcollaborative.org/home/shared/iplantcollaborative/example_data/qxpak/input/parameterFile.par",
+#             "data_file": "agave://data.iplantcollaborative.org/home/shared/iplantcollaborative/example_data/qxpak/input/dataFile.dat",
+#             "pedigree_file": "agave://data.iplantcollaborative.org/home/shared/iplantcollaborative/example_data/qxpak/input/pedigreeFile.ped",
+#             "marker_file": "agave://data.iplantcollaborative.org/home/shared/iplantcollaborative/example_data/qxpak/input/markerFile.mkr"
+#         },
+#         "parameter": {
+#             "output": "test"
+#         }
+#
+#     }
+
+#TODO find Gemma JSON?
+# def gemma_json(data_name, input_dict):
+#     pass
+
+
+def make_gwas_json(selected_gwas, datafolder):
+    """Selected GWAS is a list of binary values with each element representing
+        whether a given gwas should be run or not.
+
+    :param selected_gwas: List of GWAS applications to run the data through
+    :return:
+    """
     pass
+
+
+def make_winnow_json(job_name, gwas_folder, ote_file):
+    """" Builds JSON files for running Winnow via Agave.
+
+    Built from JSON file here: https://raw.githubusercontent.com/CyVerse-Validate/Stampede-Files/master/json/winnow-job.json
+    :param job_name: The name of the GWAS program frmo which the output came.
+    :param gwas_folder: A folder containing GWAS outputs.
+    :param ote_file: OTE known-truth file for truth testing.
+    :return:
+    """
+
+    json_text = {
+        "jobName" : job_name,
+        "softwareName" : "Winnow-0.9.2u1",
+        "requestedTime" : "02:00:00",
+        "archive" : "true",
+        "inputs" : {
+            "Class" : ote_file,
+            "Folder" : gwas_folder
+        },
+        "parameters" : {
+            "SNP": "SNP",
+            "Score": "P",
+            "Filename": "TestResults",
+            "beta": "BETA",
+            "threshold": "0.01",
+            "seper": "whitespace",
+            "kttype": "OTE"
+        }
+    }
+
+    with open('winnow-{}.json'.format(job_name), 'w') as f:
+        json_text = json.dumps(json_text, f)
--- DevelopingRelease/PipelineDevel/pipeline.py
+++ DevelopingRelease/PipelineDevel/pipeline.py
@@ -1,5 +1,6 @@
 import argparse, os, time
-from AgavePythonSDK import JobsApi
+import JsonBuilder
+import AgavePythonSDK as Agave
 
 __author__ = "Michael J. Suggs::mjs3607@uncw.edu"
 __credits__ = ["Michael Suggs"]
@@ -11,16 +12,21 @@ __status__ = "Development"
 
 # TODO: Move pipeline to a new directory to contain all outputs
 class Pipeline:
-    def __init__(self):
-        # Running jobs dictionary with the format { 'id' : 'outputPath' }
-        self.running_jobs = {}
+    """Handles job submission for the Validate Pipeline."""
+    # TODO Change pipeline around to take user folder location
 
-        # Finished jobs dictionary with the format { 'id' : list[RemoteFile] }
-        # The remote file list gives the relative path of output files.
-        self.finished_jobs = {}
-        self.submission_dir = os.getcwd()
+    def __init__(self):
+        self.data_folder = ""       # User defined folder from the Datastore
+        self.inputs = {}
+        self.known_truth = ""
+        self.dataset_name = ""
+        self.desired_gwas = ()      # Tuple of booleans
+        self.running_jobs = {}      # Running jobs dictionary with the format { 'id' : 'outputPath' }
+        self.finished_jobs = {}     # Finished jobs dictionary with the format { 'id' : list[RemoteFile] }
 
         self.checkArgs()
+
+        # TODO build gwas files from the given folder.
         self.gwas_submission()
         self.poll_jobs()
 
@@ -30,9 +36,101 @@ class Pipeline:
         :return:
         """
         parser = argparse.ArgumentParser()
-        parser.add_argument("-g", "--gwas", help="GWAS JSON files for Agave", required=True)
+        # parser.add_argument("-g", "--gwas", help="GWAS JSON files for Agave", required=True)
+        # parser.add_argument("-t", "--OTE", help="OTE file location on the Discovery Environment", required=True)
+        parser.add_argument("-i", "--InFormat", type=chr(1),
+                            help="Input format:\n"
+                                 "\tp for PED/MAP\n"
+                                 "\tb for BIM/BED/FAM\n"
+                                 "\tt for TPED/TFAM")
+        parser.add_argument("-f", "--Folder", type=str,
+                            help="Folder to be pipelined.")
+        parser.add_argument("-l", "--fastlmm", type=bool,
+                            help="\"True\" if FaST-LMM is to be run.")
+        parser.add_argument("-r", "--ridge", type=bool,
+                            help="\"True\" if Ridge is to be run.")
+        parser.add_argument("-b", "--bayes", type=bool,
+                            help="\"True\" if BayesR is to be run.")
+        parser.add_argument("-p", "--plink", type=bool,
+                            help="\"True\" if PLINK is to be run.")
+        parser.add_argument("-q", "--qxpak", type=bool,
+                            help="\"True\" if QxPak is to be run.")
+        parser.add_argument("-g", "--gemma", type=bool,
+                            help="\"True\" if Gemma is to be run.")
         args = parser.parse_args()
-        self.gwas_json = args.gwas
+        # self.gwas_json = args.gwas
+        # self.ote_location = args.ote
+        self.input_format = args.InFormat
+        self.data_folder = args.Folder
+        self.desired_gwas = tuple([args.lmm, args.ridge, args.bayes, args.plink,
+                                   args.qxpak, args.gemma])
+
+    def parse_inputs(self):
+        file_list = Agave.FilesApi.listOnDefaultSystem(self.data_folder).result
+
+        if self.input_format == 'p':
+            for file in file_list:
+                if ".ote" in file.name:
+                    self.known_truth = file.path
+                elif ".ped" in file.name:
+                    self.inputs['inputPED'] = file.path
+                elif ".map" in file.name:
+                    self.inputs['inputMAP'] = file.path
+        elif self.input_format == 'b':
+            for file in file_list:
+                if ".ote" in file.name:
+                    self.known_truth = file.path
+                elif '.bed' in file.name:
+                    self.inputs['inputBED'] = file.path
+                elif '.bim' in file.name:
+                    self.inputs['inputBIM'] = file.path
+                elif 'fam' in file.name:
+                    self.inputs['inputFAM'] = file.path
+        else:
+            for file in file_list:
+                if ".ote" in file.name:
+                    self.known_truth = file.path
+                elif ".tped" in file.name:
+                    self.inputs['inputTPED'] = file.path
+                elif ".tmap" in file.name:
+                    self.inputs['inputTMAP'] = file.path
+
+        # for file in file_list:
+        #     if ".ote" in file.name:
+        #         self.known_truth = file.path
+        #     elif self.input_format == 'p':
+        #         self.inputs['inputPED'] =
+        #         self.inputs['inputMAP'] =
+        #     elif self.input_format == 'b':
+        #         self.inputs['inputBED'] =
+        #         self.inputs['inputBIM'] =
+        #         self.inputs['inputFAM'] =
+        #     elif self.input_format == 't':
+        #         self.inputs['inputTPED'] =
+        #         self.inputs['inputTFAM'] =
+            # elif any([x in file.name for x in [".ped", ".map"]]):
+            #     self.dataset_name, tmp = file.split(".")
+            #     self.inputs.append(file)
+            # elif any([x in file.name for x in [".bim", ".bam", ".fam"]]):
+            #     self.dataset_name, tmp = file.split(".")
+            #     self.inputs.append(file)
+            # elif any([x in file.name for x in [".tped", ".tfam"]]):
+            #     self.dataset_name, tmp = file.split(".")
+            #     self.inputs.append(file)
+
+        # for file in file_list:
+        #     if file.type.lower() == "file":
+        #         name, ext = file.name.split(".")
+        #         if ext.lower() == ".ote":
+        #             self.known_truth = file
+        #         elif ext
+
+    def build_jsons(self):
+        self.gwas_jsons = []
+        for gwas in self.desired_gwas:
+            if gwas:
+                self.gwas_jsons += JsonBuilder.make_gwas_json(self.desired_gwas.index(gwas),
+                                                              self.data_folder)
 
     def gwas_submission(self):
         """Submits all provided JSON files via the Agave REST APIs.
@@ -40,13 +138,14 @@ class Pipeline:
         :return:
         """
         for json in self.gwas_json:
-            job = JobsApi.submit(json).result
+            job = Agave.JobsApi.submit(json).result
             self.running_jobs[job.id] = job.outputPath
 
     def poll_jobs(self):
         """Polls all running jobs for status until completion.
         Jobs are removed once they are finished.
         """
+        #TODO look at notifications instead?
         sleep_time = 60
         os.makedirs("Outputs")
         os.chdir("Outputs")
@@ -54,12 +153,14 @@ class Pipeline:
         # Iterating through all JobIDs and polling until there are no more jobs
         while not self.running_jobs.keys():
             for job_id in self.running_jobs:
-                job_status = JobsApi.getStatus(job_id)
+                job_status = Agave.JobsApi.getStatus(job_id)
 
                 # If the curernt given job is finished, download output and
                 # remove it from the running queue. The finished job and its
                 # output list is added to the finished_jobs dictionary.
                 if (job_status.result.status == "FINISHED") or (job_status.status == "FINISHED"):
+                    # TODO use archive instead of downloading
+                    # agave://data.iplantcollaborative.org/<user-home>/archive/jobs/job-<jobID>
                     self.download_outputs(job_id)
 
             # Sleep before repolling Agave. Max sleep time is 1 hour.
@@ -69,18 +170,24 @@ class Pipeline:
         os.chdir(self.submission_dir)
 
     def download_outputs(self, job_id):
+        #TODO avoid - work via Agave & Discovery Environment instead
         # Make a new directory for the current job's output
         if not os.path.exists():
             os.makedirs(job_id)
         os.chdir(job_id)
 
         # Getting the list of outputs for a given job and downloading
-        self.finished_jobs[job_id] = JobsApi.listOutputs(job_id, self.running_jobs[job_id])
-        JobsApi.downloadOutput(job_id)
+        self.finished_jobs[job_id] = Agave.JobsApi.listOutputs(job_id, self.running_jobs[job_id])
+        Agave.JobsApi.downloadOutput(job_id)
         del self.running_jobs[job_id]
 
         os.chdir("../")
 
+
+    def create_winnow_jsons(self):
+        # Files are located in /iplant/home/<user-dir>/archive/jobs/job-<jobid>
+        for jobid, remotelist in self.finished_jobs:
+            JsonBuilder.make_winnow_json(jobid, gwas_folder=, ote_file=)
 
     def winnow_submission(self):
         #FaST-LMM uses the base filename for the input files for its output
@@ -96,9 +203,6 @@ class Pipeline:
         # QxPak single output file - qxpak.out.
         #
         # Gemma
-        pass
-
-    def create_winnow_jsons(self):
         pass
 
     def demonstrate_submission(self):
